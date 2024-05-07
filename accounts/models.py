d542a91844da0425a7bbf52bb2a308ba7bfe1981
Yasuhiro <yasuhiro.corp@gmail.com>
2024-05-08 01:25:15 +0300

    Adding one more app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, null=False, blank=True)
